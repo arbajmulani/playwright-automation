a221d4be9a3168d7bc49640e75478308ea89f585
arbajmulani <arbajmulani732@gmail.com>
2026-01-10 16:30:29 +0530

    Removed Home Page Testcase

--- tests/home.spec.js
+++ tests/home.spec.js
@@ -2,15 +2,15 @@ import { test, expect } from '../tests/baseTest.js';
 import { LoginPage } from '../pages/LoginPage.js';
 import { envLoginData } from '../utils/testData.js';
 
-test('[@smoke] Valid login with ENV credentials', async ({ page }) => {
-  const loginPage = new LoginPage(page);
+// test('[@smoke] Valid login with ENV credentials', async ({ page }) => {
+//   const loginPage = new LoginPage(page);
 
-  await loginPage.navigate();
-  await loginPage.login(
-    envLoginData.username,
-    envLoginData.password
-  );
+//   await loginPage.navigate();
+//   await loginPage.login(
+//     envLoginData.username,
+//     envLoginData.password
+//   );
 
-  const message = await loginPage.getFlashMessage();
-  expect(message).toContain(envLoginData.successMessage);
-});
+//   const message = await loginPage.getFlashMessage();
+//   expect(message).toContain(envLoginData.successMessage);
+// });
